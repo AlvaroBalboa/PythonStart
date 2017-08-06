735e8005ae581dbb6e683f8438e774cd58805142
Alvaro Balboa <AlvaroFBalboa@outlook.com>
2017-08-06 10:55:37 -0400

    I will be adding to the main branch with new .py files for a more comprehensive overview of what I learned

---
+++ newStart.py
@@ -0,0 +1,5 @@
+print("we are now working with comments") #I think this is a comment
+'''This is a multi line comment
+I dont know why it would use completely different sign/ character to make these but I just have to remember not 
+try and figure everything out just now
+'''
