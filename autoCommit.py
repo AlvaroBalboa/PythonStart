f9ca69dca0ab245fc2615d56cca6a43307cf0da7
Alvaro Balboa <AlvaroFBalboa@outlook.com>
2017-08-15 11:23:24 -0400

    Project starting paths

---
+++ autoCommit.py
@@ -0,0 +1,9 @@
+# This I can really use
+# I want this program to be a small script that auto:
+# git add .
+# git commit
+# git push
+#
+# research system file management and operations techniques
+# assign variables to all files involved
+# have multiple variables possible with the script editing
---
+++ restart.py
@@ -0,0 +1,7 @@
+# Lets make a program that restarts this file if for some reason the application shuts down
+#
+# first start with importing the system actions
+# assign variables to all the files involved
+# research best practices in calling programs and "switch" they can have
+
+
